7b06b7d587bcee8d991b7610a960b550bcb1e2d2
pg <misterprem@gmail.com>
2016-04-22 18:29:56 +0530

    fixes

---
+++ lc/src/main/java/org/spo/svc2/trx/def/ConstantsImpl.java
@@ -0,0 +1,26 @@
+package org.spo.svc2.trx.def;
+
+import org.spo.ifs2.template.web.Constants;
+import org.springframework.stereotype.Component;
+@Component
+public class ConstantsImpl implements Constants {
+
+	public String getRepoPath() {
+		if(!System.getProperty("os.name").startsWith("Windows")){
+			return "/usr/local/share/data-cms/lc";
+		}else{
+			return "C:\\Users\\premganesh\\git\\bg1_3\\src\\main\\resources\\data-cms";
+		}
+	}
+
+	public String getLandingPage() {
+		return "trx/M01/LA01T";
+	}
+
+	public int getPortNumber() {
+		// TODO Auto-generated method stub
+		return 8089;
+	}
+
+}
+
